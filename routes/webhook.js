db73667dd7eefed86be62e7fbbcc60aebcdb025b
taejune_ahn <voidmain0805@gmail.com>
2020-07-14 16:54:15 +0900

    Add trigger yaml for l2c

--- routes/webhook.js
+++ routes/webhook.js
@@ -2,41 +2,39 @@ const process = require('process');
 const url = require("url");
 var fetch = require("node-fetch");
 var express = require('express');
+var fs = require('fs');
 var router = express.Router();
 
 const execSync = require('child_process').execSync
 
 router.post('/listen', function (req, res, next) {
-    console.log(res.json().body)
 
     const analysisResult = res.json().body.status
     if(analysisResult === 'SUCCESS') {
+        console.log('Sonnar report Success')
         execMigrate()
     } else {
+        console.log('Sonnar report Fail')
         deployIDE()
     }
 });
 
-router.get('/test', function (req, res, next) {
-    console.log("test")
-    execMigrate()
-    .then(data => {
-        console.log(data);
-    })
-    .catch(e => {
-        console.error(e)
-    })
-});
-
-
 function execMigrate() {
-    console.log("kubectl create -f test/test-service.yaml")
-    const stdout = execSync("kubectl create -f test/test-service.yaml")
+    const manifestPath = "/tmp/l2c-run-instance.yaml"
+
+    if(!fs.existsSync(manifestPath)) {
+        console.error('Cannot found manifest file: ' + manifestPath)
+        return false
+    }
+
+    const stdout = execSync(`kubectl create -f ${manifestPath}`)
     console.log(stdout.toString())
+    
+    return true
 }
 
 function deployIDE() {
-
+    console.log("not implmeneted yet...")
 }
 
 module.exports = router;
