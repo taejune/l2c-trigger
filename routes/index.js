0db741f903b3fffcf4f6898bf697d99f878c493b
taejune_ahn <voidmain0805@gmail.com>
2020-07-20 09:48:05 +0900

    Update script executed by sh

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,15 @@
 const process = require('process');
-const util = require('util')
 const fs = require('fs')
 const { spawn } = require('child_process')
 
 var express = require('express');
 var router = express.Router();
 
-const access = util.promisify(fs.access)
-const chmod = util.promisify(fs.chmod)
-
 router.post('/', function (req, res, next) {
 
-    console.log(`Receive message from sonarqube: \n ${JSON.stringify(req.body)}`)
+    console.log(`Receive message from sonarqube:`)
+    console.log(`${JSON.stringify(req.body, null, 2)}`)
+    
     const analysisResult = req.body.qualityGate.status
     if (analysisResult === 'SUCCESS') {
         execScript(process.env.SUCCESS_HANDLER_PATH)
@@ -24,19 +22,20 @@ router.post('/', function (req, res, next) {
 
 async function execScript(path) {
     try {
-        await access(path)
-        await chmod(path, 0o755)
+        const child = spawn('sh', [`${path}`])
 
-        const child = spawn(`./${path}`)
         child.stdout.on('data', (data) => {
             console.log(`${data}`)
         })
+
         child.stderr.on('data', (data) => {
             console.log(`${data}`)
         })
+
         child.on('close', (exitcode) => {
             console.log(`process exited with code ${exitcode}`)
         })
+
     } catch (e) { throw e }
 }
 
